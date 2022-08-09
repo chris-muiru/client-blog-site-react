159632db48a3ae520c0d3a16df37a5c53514ae91
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-09 20:10:40 +0300

    add: add single blog component

---
+++ src/Components/Dashboard/Blog.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Blog = () => {
+	return (
+		<div className="bg-white relative min-h-24 w-3/4 m-auto rounded-sm p-6">
+			<h1 className="font-bold text-2xl">The art of war</h1>
+			<p className="">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+				corrupti ducimus sequi laboriosam eum cupiditate possimus aliquid
+				quaerat ad omnis expedita, rerum ipsam, distinctio deserunt
+				recusandae ab reprehenderit accusantium enim!
+			</p>
+			<small className="absolute bottom-0 ">by kris</small>
+		</div>
+	)
+}
+
+export default Blog
