00703fcebced5240e52ed9d61485d4dac7a29043
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-09 20:11:57 +0300

    add: BlogGroup component that holds group of blogs

---
+++ src/Components/Dashboard/BlogGroup.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import Blog from "./Blog"
+
+const BlogGroup = () => {
+	return (
+		<div className="flex w-3/4 m-auto flex-wrap space-y-10 mt-4">
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+			<Blog />
+		</div>
+	)
+}
+
+export default BlogGroup
