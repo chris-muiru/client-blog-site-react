0fffa6a6228926b18b57951090f98c9f359a7b17
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 01:48:16 +0300

    add: authRoutes

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import LogIn from "../Components/LogIn/LogInView"
-import SignUp from "../Components/LogIn/SignUp"
+import SignUpView from "../Components/LogIn/SignUpView"
 
 const AuthRoutes = () => {
 	return (
 		<Routes>
-			<Route path="/signUp" element={<SignUp />} />
+			<Route path="/signUp" element={<SignUpView />} />
 			<Route path="/signIn" element={<LogIn />} />
 		</Routes>
 	)
