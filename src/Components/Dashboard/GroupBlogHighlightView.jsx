1196129e6ab28ff45848c941e1af08c11fe4e36a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 01:40:27 +0300

    add: Group Blog Hightlight to hold many blog highlight views

---
+++ src/Components/Dashboard/GroupBlogHighlightView.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import BlogHighlight from "./BlogHighlightView"
+
+const GroupBlogHighlightView = () => {
+	return (
+		<div className="flex w-3/4 m-auto flex-wrap space-y-10 mt-4">
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+			<BlogHighlight />
+		</div>
+	)
+}
+
+export default GroupBlogHighlightView
