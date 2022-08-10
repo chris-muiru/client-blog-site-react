ee98fcbf8255e191762cc2841c55c7ee3bae8075
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 01:48:33 +0300

    add: dashboard routes

--- src/routes/DashRoutes.jsx
+++ src/routes/DashRoutes.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import CreateBlog from "../Components/Dashboard/CreateBlog"
-import Dash from "../Components/Dashboard/Dash"
-
+import CreateBlog from "../Components/Dashboard/CreateBlogView"
+import Dash from "../Components/Dashboard/DashView"
+import BlogView from "../Components/Dashboard/BlogView"
 const DashRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Dash />} />
 			<Route path="/new" element={<CreateBlog />} />
+			<Route path="/blog" element={<BlogView />} />
 		</Routes>
 	)
 }
