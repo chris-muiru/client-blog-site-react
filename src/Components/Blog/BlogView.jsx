2e8bec364a6b990d64c34f23e8c76176b87b7c80
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 18:57:05 +0300

    mv: from Dashboard to Blog dir

---
+++ src/Components/Blog/BlogView.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import NavBarView from "../Dashboard/NavBarView"
+import CommentView from "./CommentView"
+import LikeView from "./LikeView"
+
+const BlogView = () => {
+	return (
+		<>
+			<NavBarView />
+			<div className="w-3/4 relative  m-auto">
+				<LikeView />
+				<div className="bg-slate-100 min-h-[200px] mt-10 m-auto rounded-md">
+					<p className="text-center font-bold text-md text-yellow-600 pt-3">
+						This is the heading
+					</p>
+					<div className="p-10 text-slate-900 font-semibold">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Possimus dolor enim eos temporibus odit amet quia dolores
+						voluptate. Fugiat, autem? Optio amet suscipit ipsa repellat
+						non, neque commodi libero doloremque
+					</div>
+				</div>
+				{/* <CommentView /> */}
+			</div>
+		</>
+	)
+}
+
+export default BlogView
