2c4fcdbb60d391d5dc5183b7d0a269158ec556f1
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 20:53:38 +0300

    update: add tailwind styles

--- src/Components/Dashboard/BlogHighlightView.jsx
+++ src/Components/Dashboard/BlogHighlightView.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const BlogHighlightView = () => {
 	return (
 		<Link to="/blog">
-			<div className="bg-slate-100 relative min-h-[204px] w-3/4 m-auto rounded-sm p-6">
+			<div className="bg-slate-100 text-slate-900 relative min-h-[204px] w-3/4 m-auto rounded-sm p-6">
 				<h1 className="font-bold text-2xl text-yellow-600">The art of war</h1>
-				<p className="">
+				<p className="font-semibold">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
 					corrupti ducimus sequi laboriosam eum cupiditate possimus aliquid
 					quaerat ad omnis expedita, rerum ipsam, distinctio deserunt
