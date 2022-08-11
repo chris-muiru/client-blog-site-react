c1ddc45c0a3e43521a9764dc529e908c08692a6e
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 18:58:09 +0300

    add: mv from Dash to blog dir

---
+++ src/Components/Blog/CreateBlogView.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import NavBar from "../Dashboard/NavBarView"
+import LikeView from "./LikeView"
+
+const CreateBlogView = () => {
+	return (
+		<div>
+			<NavBar />
+			<div className="flex flex-col w-1/2 m-auto ">
+				<form action="" class="flex  flex-col items-center space-y-4 mt-5">
+					<input
+						type="text"
+						placeholder="Title"
+						className="pl-10 text-white w-full h-24 rounded-md bg-slate-700"
+					/>
+					<input
+						type="text"
+						placeholder="Genre"
+						className="pl-10  text-white w-full h-24  rounded-md bg-slate-700"
+					/>
+					<textarea
+						type="text"
+						placeholder="Enter text here...."
+						className="pl-10  text-white w-full min-h-[400px] rounded-md bg-slate-700"
+					></textarea>
+					<button
+						type="submit"
+						class="text-white bg-blue-600 w-40 p-4 rounded-md "
+					>
+						Create
+					</button>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default CreateBlogView
