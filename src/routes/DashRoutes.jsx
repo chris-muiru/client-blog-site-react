66045eb08d85a34351e7a7442ff83e00f5c5855b
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 20:56:42 +0300

    add: AboutView route

--- src/routes/DashRoutes.jsx
+++ src/routes/DashRoutes.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom"
 import CreateBlog from "../Components/Blog/CreateBlogView"
 import Dash from "../Components/Dashboard/DashView"
 import BlogView from "../Components/Blog/BlogView"
+import AboutView from "../Components/About/AboutView"
 const DashRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Dash />} />
 			<Route path="/new" element={<CreateBlog />} />
 			<Route path="/blog" element={<BlogView />} />
+			<Route path="/about" element={<AboutView/>} />
 		</Routes>
 	)
 }
