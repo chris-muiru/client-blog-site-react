a6d0f2fefe41ecfce96171f7eda8ad6c2aa00224
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-11 18:59:30 +0300

    add: comment view

---
+++ src/Components/Blog/CommentView.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const CommentView = () => {
+	return (
+		<div className="bg-slate-900 min-h-[24px] w-full">
+			<div>
+				
+			</div>
+		</div>
+	)
+}
+
+export default CommentView
