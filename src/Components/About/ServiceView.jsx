dd28116158e4e33f8a18678bd56573c656a17a42
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-12 23:05:32 +0300

    add: Service view

---
+++ src/Components/About/ServiceView.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import SharedView from "./SharedView"
+
+const ServiceView = () => {
+	return (
+		<>
+			<SharedView heading="What we do">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo amet,
+				corporis voluptate suscipit minus consectetur tempora sequi
+				veritatis explicabo, alias, officia mollitia debitis illo nostrum
+				magnam magni nam aut? Quam.
+			</SharedView>
+		</>
+	)
+}
+
+export default ServiceView
