1105c0e0f648547fd32da49c000759bf62a56565
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-12 23:02:04 +0300

    add: Main component import

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from "react-router-dom"
 import AuthRoutes from "./routes/AuthRoutes"
 import DashRoutes from "./routes/DashRoutes"
 import "./dist/output.css"
+import Main from "./Main"
 function App() {
 	return (
 		<div className="bg-slate-800 min-h-screen">
 			<BrowserRouter>
 				<AuthRoutes />
-				<DashRoutes />
+				{/* <DashRoutes /> */}
+				<Main />
 			</BrowserRouter>
 		</div>
 	)
