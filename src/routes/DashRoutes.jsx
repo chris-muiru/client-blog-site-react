f41171b61308a9a1a8445ccf8e3c33025117c772
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-13 00:39:01 +0300

    add: service and contact route

--- src/routes/DashRoutes.jsx
+++ src/routes/DashRoutes.jsx
@@ -4,13 +4,17 @@ import CreateBlog from "../Components/Blog/CreateBlogView"
 import Dash from "../Components/Dashboard/DashView"
 import BlogView from "../Components/Blog/BlogView"
 import AboutView from "../Components/About/AboutView"
+import ServiceView from "../Components/About/ServiceView"
+import ContactView from "../Components/About/ContactView"
 const DashRoutes = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Dash />} />
 			<Route path="/new" element={<CreateBlog />} />
 			<Route path="/blog" element={<BlogView />} />
-			<Route path="/about" element={<AboutView/>} />
+			<Route path="/about" element={<AboutView />} />
+			<Route path="/service" element={<ServiceView />} />
+			<Route path="/contact" element={<ContactView />} />
 		</Routes>
 	)
 }
