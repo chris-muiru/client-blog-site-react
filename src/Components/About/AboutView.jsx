6c87679eabfa873366033dee2d1645020b4d6df1
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-15 01:30:11 +0300

    add: navbarview component

--- src/Components/About/AboutView.jsx
+++ src/Components/About/AboutView.jsx
@@ -1,11 +1,11 @@
 import React from "react"
 import NavBarView from "../Dashboard/NavBarView"
-import SocialsIcons from "../Socials/SocialsIcons"
 import SharedView from "./SharedView"
 
 const AboutView = () => {
 	return (
 		<>
+			<NavBarView />
 			<SharedView heading="What we do">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
 				asperiores quisquam labore unde voluptatem velit quo enim ullam,
