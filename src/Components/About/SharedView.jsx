6c60b3b7b1c91ed932232e1f04b661ec24ee7794
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-25 01:51:58 +0300

    update style

--- src/Components/About/SharedView.jsx
+++ src/Components/About/SharedView.jsx
@@ -4,7 +4,7 @@ import SocialsIcons from "../Socials/SocialsIcons"
 const SharedView = ({ heading, children }) => {
 	return (
 		<div>
-			<div className="rounded-md bg-white min-h-[200px] font-semibold w-3/4 container m-auto pt-3 mt-14">
+			<div className="rounded-md bg-white min-h-[200px] font-semibold w-11/12 sm:w-3/4 container m-auto pt-3 mt-14">
 				<p className="text-yellow-600 font-bold text-center">{heading}</p>
 
 				<div className="p-10 text-slate-900 font-semibold">{children}</div>
