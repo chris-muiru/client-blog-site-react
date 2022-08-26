1719305197e5ebaaa849458315324bf4afa95f95
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 02:46:28 +0300

    update: change location of button

--- src/Components/Dashboard/NavBarView.jsx
+++ src/Components/Dashboard/NavBarView.jsx
@@ -1,14 +1,40 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import logo from "./../../logo/transparent-eagle-slinger.svg"
+import SideNavView from "./SideNavView"
 const NavBarView = () => {
 	return (
-		<div className="bg-gray-900">
+		<div className="bg-gray-900 min-h-[100px]">
 			<nav className="px-2 py-2.5 sm:px-4">
 				<div className="container flex flex-wrap justify-between items-center mx-auto">
-					<div className="flex items-center">
-						<img src={logo} className="h-24" alt="slinger logo" />
-						<span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+					<button
+						data-collapse-toggle="navbar-cta"
+						type="button"
+						className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+						aria-controls="navbar-cta"
+						aria-expanded="false"
+					>
+						<svg
+							className="w-6 h-6"
+							aria-hidden="true"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+								clip-rule="evenodd"
+							></path>
+						</svg>
+					</button>
+					<div className="relative w-40">
+						<img
+							src={logo}
+							className="h-24 inline-block"
+							alt="slinger logo"
+						/>
+						<span className=" absolute top-8 right-7 text-xl font-semibold text-white">
 							slinger
 						</span>
 					</div>
@@ -21,29 +47,6 @@ const NavBarView = () => {
 								Create Blog
 							</button>
 						</Link>
-
-						<button
-							data-collapse-toggle="navbar-cta"
-							type="button"
-							className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-							aria-controls="navbar-cta"
-							aria-expanded="false"
-						>
-							<span className="sr-only">Open main menu</span>
-							<svg
-								className="w-6 h-6"
-								aria-hidden="true"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-									clip-rule="evenodd"
-								></path>
-							</svg>
-						</button>
 					</div>
 					<div
 						className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
