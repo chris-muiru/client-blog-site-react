a5a526087f2cdd7e77107b0e36e1543e5091828e
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 02:43:06 +0300

    add: fetchblogs api and use a map function to load response to bloghighlight component

--- src/Components/Dashboard/GroupBlogHighlightView.jsx
+++ src/Components/Dashboard/GroupBlogHighlightView.jsx
@@ -1,19 +1,42 @@
 import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+import { useAuthContext } from "../../context/AuthContextProvider"
 import BlogHighlight from "./BlogHighlightView"
+const URL = "http://localhost:8000/blog/"
 
 const GroupBlogHighlightView = () => {
+	const [blogs, setBlogs] = useState(null)
+	const { getAuthToken } = useAuthContext()
+	const fetchBlogs = async () => {
+		const response = await fetch(URL, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${getAuthToken()}`,
+			},
+		})
+		const dataJson = await response.json()
+		setBlogs(dataJson)
+	}
+	useEffect(() => {
+		fetchBlogs()
+	}, [])
+	console.log(blogs)
 	return (
-		<div className="flex m-auto  w-11/12 sm:w-7/12  flex-wrap space-y-10 mt-4 ">
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
-			<BlogHighlight />
+		<div className="flex m-auto  w-11/12 sm:w-7/12  flex-wrap space-y-10 mt-4">
+			{blogs &&
+				blogs.map(({ title, content, blogtype }) => {
+					console.log(title)
+
+					return (
+						<BlogHighlight
+							title={title}
+							content={content}
+							blogtype={blogtype}
+						/>
+					)
+				})}
 		</div>
 	)
 }
