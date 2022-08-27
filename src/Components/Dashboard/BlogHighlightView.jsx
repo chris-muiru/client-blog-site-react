af5b5cbfd977a3bfef521c5c7677997037d597e9
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 15:59:34 +0300

    add: link to navigate to blog via its id

--- src/Components/Dashboard/BlogHighlightView.jsx
+++ src/Components/Dashboard/BlogHighlightView.jsx
@@ -1,15 +1,15 @@
 import React from "react"
 import { Link } from "react-router-dom"
-const BlogHighlightView = ({ title, content }) => {
+const BlogHighlightView = ({ title, content, blogId }) => {
 	// console.log(title)
 	return (
-			<Link to="/blog" className="w-full">
-				<div className="bg-slate-100 text-slate-900 relative min-h-[204px] m-auto rounded-sm p-6 w-full">
-					<h1 className="font-bold text-2xl text-yellow-600">{title}</h1>
-					<p className="font-semibold">{content}</p>
-					<small className="absolute bottom-0">by kris</small>
-				</div>
-			</Link>
+		<Link to={`/blog/${blogId}`} className="w-full">
+			<div className="bg-slate-100 text-slate-900 relative min-h-[204px] m-auto rounded-sm p-6 w-full">
+				<h1 className="font-bold text-2xl text-yellow-600">{title}</h1>
+				<p className="font-semibold">{content}</p>
+				<small className="absolute bottom-0">by kris</small>
+			</div>
+		</Link>
 	)
 }
 
