7190e11f5c5d46ce05a8443e21b1af3d3ed85a72
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 15:57:20 +0300

    add: fetchBlogByid functionality and a map function to display the feched data

--- src/Components/Blog/BlogView.jsx
+++ src/Components/Blog/BlogView.jsx
@@ -1,24 +1,52 @@
 import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useAuthContext } from "../../context/AuthContextProvider"
 import NavBarView from "../Dashboard/NavBarView"
 import CommentView from "./CommentView"
 import LikeView from "./LikeView"
 
 const BlogView = () => {
+	let { getAuthToken } = useAuthContext()
+	let { blogId } = useParams()
+	let [blog, setBlog] = useState(null)
+	let BlogUrl = `http://localhost:8000/blog/${blogId}`
+	const fetchBlogById = async () => {
+		let response = await fetch(BlogUrl, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${getAuthToken()}`,
+			},
+		})
+		let dataJson = await response.json()
+		setBlog([dataJson])
+		console.log(dataJson)
+	}
+	console.log(blog)
+	useEffect(() => {
+		fetchBlogById()
+	}, [])
 	return (
 		<>
 			<NavBarView />
 			<div className="w-11/12 sm:w-3/4 relative  m-auto">
 				<LikeView />
 				<div className="bg-slate-100 min-h-[200px] mt-10 m-auto rounded-md">
-					<p className="text-center font-bold  text-yellow-600 pt-3">
-						This is the heading
-					</p>
-					<div className="p-4 sm:p-10 text-slate-900 font-semibold">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Possimus dolor enim eos temporibus odit amet quia dolores
-						voluptate. Fugiat, autem? Optio amet suscipit ipsa repellat
-						non, neque commodi libero doloremque
-					</div>
+					{blog &&
+						blog.map(({ title,content }) => {
+							return (
+								<div>
+									<p className="text-center font-bold  text-yellow-600 pt-3">
+										{title}
+									</p>
+									<div className="p-4 sm:p-10 text-slate-900 font-semibold">
+										{content}
+									</div>
+								</div>
+							)
+						})}
 				</div>
 				{/* <CommentView /> */}
 			</div>
