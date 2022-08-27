fc8f086a45ada388bfc80a53ba4e02e8a32d09ff
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 21:30:40 +0300

    add: form to post created blog entries

--- src/Components/Blog/CreateBlogView.jsx
+++ src/Components/Blog/CreateBlogView.jsx
@@ -1,23 +1,55 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useAuthContext } from "../../context/AuthContextProvider"
 import NavBarView from "../Dashboard/NavBarView"
 
 const CreateBlogView = () => {
+	const { getAuthToken } = useAuthContext()
+	const blogUrl = `http://localhost:8000/blog/`
+	const createBlog = async (e) => {
+		e.preventDefault()
+		let response = await fetch(blogUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${getAuthToken()}`,
+			},
+			body: JSON.stringify({
+				title: e.target.title.value,
+				blogType: e.target.blogType.value,
+				content: e.target.content.value,
+			}),
+		})
+		let dataJson = await response.json()
+		console.log(dataJson)
+		window.location = "/"
+	}
+
+	useEffect(() => {
+		createBlog()
+	}, [])
 	return (
 		<div>
 			<NavBarView />
 			<div className="flex flex-col m-4 sm:w-1/2 sm:m-auto ">
-				<form action="" class="flex  flex-col items-center space-y-4 mt-5">
+				<form
+					method="post"
+					onSubmit={createBlog}
+					class="flex  flex-col items-center space-y-4 mt-5"
+				>
 					<input
+						name="title"
 						type="text"
 						placeholder="Title"
 						className="pl-10 text-white w-full h-24 rounded-md bg-slate-700"
 					/>
 					<input
+						name="blogType"
 						type="text"
 						placeholder="Genre"
 						className="pl-10  text-white w-full h-24  rounded-md bg-slate-700"
 					/>
 					<textarea
+						name="content"
 						type="text"
 						placeholder="Enter text here...."
 						className="pl-10  text-white w-full min-h-[400px] rounded-md bg-slate-700"
