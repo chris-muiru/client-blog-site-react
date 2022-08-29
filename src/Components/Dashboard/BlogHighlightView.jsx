86b57c2ba3234f1e5e7882d7ca0164a71aa4f489
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-29 15:22:08 +0300

    add: date functionality

--- src/Components/Dashboard/BlogHighlightView.jsx
+++ src/Components/Dashboard/BlogHighlightView.jsx
@@ -1,13 +1,40 @@
 import React from "react"
 import { Link } from "react-router-dom"
-const BlogHighlightView = ({ title, content, blogId }) => {
-	// console.log(title)
+const BlogHighlightView = ({
+	title,
+	content,
+	blogId,
+	createdAt,
+	writterName,
+}) => {
+	let date = new Date(createdAt)
+	let months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"June",
+		"July",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec",
+	]
 	return (
 		<Link to={`/blog/${blogId}`} className="w-full">
 			<div className="bg-slate-100 text-slate-900 relative min-h-[204px] m-auto rounded-sm p-6 w-full">
 				<h1 className="font-bold text-2xl text-yellow-600">{title}</h1>
-				<p className="font-semibold">{content}</p>
-				<small className="absolute bottom-0">by kris</small>
+				<small className="absolute right-0 top-7 text-green-700">{`
+				${date.getDate()} 
+				${months[date.getMonth()]}, ${date.getFullYear()}`}</small>
+				<p className="font-semibold text-justify">
+					{content.substr(0, 800)}...
+				</p>
+				<p className="absolute font-bold bottom-0 text-green-700">
+					by {writterName}
+				</p>
 			</div>
 		</Link>
 	)
