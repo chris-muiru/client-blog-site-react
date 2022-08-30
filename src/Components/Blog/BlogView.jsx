fda6c52378686711efe078469491d6bbeef6cab5
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-30 23:36:16 +0300

    add: functionality to only display delete blog icon only if currently logged in writter created the blog

--- src/Components/Blog/BlogView.jsx
+++ src/Components/Blog/BlogView.jsx
@@ -5,10 +5,12 @@ import { useParams } from "react-router-dom"
 import { useAuthContext } from "../../context/AuthContextProvider"
 import NavBarView from "../Dashboard/NavBarView"
 import CommentView from "./CommentView"
+import DeleteBlogView from "./DeleteBlogView"
 import LikeView from "./LikeView"
 
 const BlogView = () => {
 	let { getAuthToken } = useAuthContext()
+	const [isBlogOwner, setIsBlogOwner] = useState(false)
 	let { blogId } = useParams()
 	let [blog, setBlog] = useState(null)
 	let BlogUrl = `http://localhost:8000/blog/${blogId}`
@@ -22,53 +24,80 @@ const BlogView = () => {
 		})
 		let dataJson = await response.json()
 		setBlog([dataJson])
-		console.log(dataJson)
 	}
+	const displayDeleteBtnIfTrue = (status) => {
+		if (status) {
+			return <DeleteBlogView />
+		}
+		return ""
+	}
+	const getBlogOwner = async (blogId) => {
+		let response = await fetch(
+			`http://localhost:8000/blog/isPermitted/${blogId}/`,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${getAuthToken()}`,
+				},
+			}
+		)
+		if (response.status === 200) {
+			let dataJson = await response.json()
+			setIsBlogOwner(dataJson)
+		} else {
+			setIsBlogOwner(false)
+		}
+	}
+	// console.log(isBlogOwner)
 	useEffect(() => {
 		fetchBlogById()
+		getBlogOwner(blogId)
 	}, [])
 	return (
 		<>
 			<NavBarView />
 			<div className="w-11/12 sm:w-3/4 relative  m-auto">
 				<LikeView />
-				<div className="bg-slate-100 min-h-[200px] mt-10 m-auto rounded-md">
-					{blog &&
-						blog.map(({ title, content, createdAt }) => {
-							let date = new Date(createdAt)
-							let months = [
-								"Jan",
-								"Feb",
-								"Mar",
-								"Apr",
-								"May",
-								"June",
-								"July",
-								"Aug",
-								"Sep",
-								"Oct",
-								"Nov",
-								"Dec",
-							]
+				<div>
+					<div className="bg-slate-100 min-h-[200px] mt-10 m-auto rounded-md">
+						{blog &&
+							blog.map(({ title, content, createdAt }) => {
+								let date = new Date(createdAt)
+								let months = [
+									"Jan",
+									"Feb",
+									"Mar",
+									"Apr",
+									"May",
+									"June",
+									"July",
+									"Aug",
+									"Sep",
+									"Oct",
+									"Nov",
+									"Dec",
+								]
 
-							return (
-								<div className="relative">
-									<p className="text-center font-bold  text-yellow-600 pt-3">
-										{title}
-									</p>
-									<small className="absolute right-0 top-7 text-green-700">
-										{`
+								return (
+									<div className="relative">
+										{displayDeleteBtnIfTrue(isBlogOwner)}
+										<p className="text-center font-bold  text-yellow-600 pt-3">
+											{title}
+										</p>
+										<small className="absolute right-0 sm:right-10 top-3 text-green-700">
+											{`
 										${date.getDate()} 
 										${months[date.getMonth()]}, ${date.getFullYear()}`}
-									</small>
-									<div className="p-4 sm:p-10 text-slate-900 font-semibold text-justify">
-										{content}
+										</small>
+										<div className="p-4 sm:p-10 text-slate-900 font-semibold text-justify">
+											{content}
+										</div>
 									</div>
-								</div>
-							)
-						})}
+								)
+							})}
+					</div>
 				</div>
-				{/* <CommentView /> */}
 			</div>
 		</>
 	)
